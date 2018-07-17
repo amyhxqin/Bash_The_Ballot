5af81dec6a54944af175ec90696c9437b9e66800
Jonathan Yang <jonathan8589@gmail.com>
2018-07-17 00:59:12 -0400

    added cors

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var cors = require('cors');
 var port = 8081;
 const path = require('path');
 
 var bodyParser = require('body-parser');  
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 var sql = require("mssql");
 // Create connection to database
